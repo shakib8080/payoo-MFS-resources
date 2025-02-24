c2d0f28581dc1d63800dc49d181f99e488d9fee3
shakib8080 <mdshakibulhasan0808@gmail.com>
2025-02-24 21:47:56 +0600

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+document
+  .getElementById("loging-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const contect = document.getElementById("contact-id").value;
+    const pin = document.getElementById("pin-id").value;
+    if (contect.length === 11) {
+      window.location.href = "./main.html";
+      if (pin === "1234") {
+        window.location.href = "./main.html";
+      } else {
+        alert("pin is incorect");
+      }
+    } else {
+      alert("please provide currect number");
+    }
+  });
